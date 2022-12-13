5a0086e79fc659fc9f0454fd6103c78d39e828e9
LabbJoil <lev.t119@gmail.com>
2022-12-13 11:29:23 +0300

    Controllers

---
+++ src/main/java/Credit_conveyor/Controllers/MultipleController.java
@@ -0,0 +1,35 @@
+package Credit_conveyor.Controllers;
+
+import Credit_conveyor.Services.Facade;
+import Credit_conveyor.model.LoanApplicationRequestDTO;
+import Credit_conveyor.model.LoanOfferDTO;
+import Credit_conveyor.model.ScoringDataDTO;
+import Credit_conveyor.model.CreditDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+@RestController
+@RequestMapping("/conveyor")
+public class MultipleController {
+    private final Facade facade;
+
+    @PostMapping("/offers")
+    public ResponseEntity<List<LoanOfferDTO>> offerController(@RequestBody LoanApplicationRequestDTO request) {
+        return ResponseEntity.ok(facade.generateOffers(request));
+    }
+
+    @PostMapping("/calculation")
+    public ResponseEntity<CreditDTO> calculationController(@RequestBody ScoringDataDTO request) {
+        return ResponseEntity.ok(facade.generateCredit(request));
+    }
+}
